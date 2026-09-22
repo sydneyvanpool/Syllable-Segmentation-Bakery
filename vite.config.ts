da5f4904469f73832537ff8b67800d49f633f891
sydneyvanpool <sydneyvanpool@dcsok.org>
2026-09-22 15:53:10 -0500

    Set base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Syllable-Segmentation-Bakery/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
